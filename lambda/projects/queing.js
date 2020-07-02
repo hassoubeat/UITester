08ac0644062b24b732ff6cbab6cafa35f4d66a09
hassoubeat <hassoubeat.work@gmail.com>
2020-07-02 11:46:18 +0900

    Update SQSキューイング登録処理の追加実装

--- lambda/projects/queing.js
+++ lambda/projects/queing.js
@@ -1,6 +1,19 @@
+const AWS = require('aws-sdk');
+const SQS = new AWS.SQS({apiVersion: '2012-11-05'});
+const SQS_QUEUE_URL = process.env.SCREENSHOT_PROCESS_SQS;
+
 exports.lambda_handler = async (event, context) => {
   inputData = JSON.parse(event['body']);
   console.log(inputData.test);
+
+  let params = {
+    MessageBody: inputData.test,
+    QueueUrl: SQS_QUEUE_URL,
+  };
+
+  result = await SQS.sendMessage(params).promise();
+  console.log(result);
+
   try {
     response = {
       'statusCode': 200,
