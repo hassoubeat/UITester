f6f617cf2872b86208ad7d62432a7d4c20127ce5
hassoubeat <hassoubeat.work@gmail.com>
2020-07-04 20:09:28 +0900

    Update DynamoDB モック処理実装

--- lambda/projects/ScreenshotProcessQueingFunction/index.js
+++ lambda/projects/ScreenshotProcessQueingFunction/index.js
@@ -1,23 +1,48 @@
 const AWS = require('aws-sdk');
 const SQS = new AWS.SQS({apiVersion: '2012-11-05'});
+const DYNAMODB = new AWS.DynamoDB.DocumentClient();
+
 const SQS_QUEUE_URL = process.env.SCREENSHOT_PROCESS_SQS;
+const DYNAMODB_TABLE_NAME = process.env.DYNAMODB_TABLE_NAME
 
 exports.lambda_handler = async (event, context) => {
-  console.log(event);
   const payload = JSON.parse(event.body);
   console.log(payload);
 
   var result = {};
 
-  // Actionの内容をSQSに登録
+  // DynamoDBにメタデータ(Result-Set)の登録
+  await DYNAMODB.put({
+    TableName: DYNAMODB_TABLE_NAME,
+    Item: {
+      Id: 'Result-Set-1',
+      Type: 'SCREENSHOT', 
+      ProjectName: payload.project.projectName
+    }
+  }).promise();
+
   for(const action of payload.project.actions) {
     console.log(action);
-    let params = {
-      MessageBody: JSON.stringify(action),
-      QueueUrl: SQS_QUEUE_URL,
-    };
+
     try {
-      result[action.actionId] = await SQS.sendMessage(params).promise();
+      // Actionの内容をSQSに登録
+      result[action.actionId] = await SQS.sendMessage({
+        MessageBody: JSON.stringify(action),
+        QueueUrl: SQS_QUEUE_URL,
+      }).promise();
+
+      // DynamoDBにメタデータ(Result)の登録
+      await DYNAMODB.put({
+        TableName: DYNAMODB_TABLE_NAME,
+        Item: {
+          Id: 'Result-1',
+          Type: 'SCREENSHOT', 
+          ResultName: action.actionName,
+          Progress: '未処理',
+          ResultSetId: 'Result-Set-1'
+        }
+      }).promise();
+
     } catch (error) {
       result[action.actionId] = error;
     }
