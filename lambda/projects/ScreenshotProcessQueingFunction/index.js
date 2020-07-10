b97883b2cf84a3c813a6cfcfa8de0bdf697d04fe
hassoubeat <hassoubeat.work@gmail.com>
2020-07-10 15:49:39 +0900

    Update DIできる形にコードを修正

--- lambda/projects/ScreenshotProcessQueingFunction/index.js
+++ lambda/projects/ScreenshotProcessQueingFunction/index.js
@@ -1,74 +1,13 @@
 const AWS = require('aws-sdk');
 const SQS = new AWS.SQS({apiVersion: '2012-11-05'});
-const SQS_QUEUE_URL = process.env.SCREENSHOT_PROCESS_SQS;
+const DYNAMODB = new AWS.DynamoDB.DocumentClient();
 
-const dynamodbDao = require('dynamodb-dao');
-const UITESTER_DYNAMODB_TABLE_NAME = process.env['UITESTER_DYNAMODB_TABLE_NAME'];
+const screenshotProcessQueing = require('./app');
 
 exports.lambda_handler = async (event, context) => {
-  const payload = JSON.parse(event.body);
-
-  var result = {};
-
-  // DynamoDBにメタデータ(Result-Set)の登録
-  const resultSetId = `Result-Set-${await dynamodbDao.getResultSetId(UITESTER_DYNAMODB_TABLE_NAME)}`
-  await dynamodbDao.put({
-    TableName: UITESTER_DYNAMODB_TABLE_NAME,
-    Item: {
-      Id: resultSetId,
-      Type: 'SCREENSHOT', 
-      ProjectName: payload.project.projectName
-    }
-  });
-
-  result[resultSetId] = {};
-
-  for(const action of payload.project.actions) {
-    try {
-      const resultId = `Result-${await dynamodbDao.getResultId(UITESTER_DYNAMODB_TABLE_NAME)}`;
-      // DynamoDBにメタデータ(Result)の登録
-      const putObject = {
-        TableName: UITESTER_DYNAMODB_TABLE_NAME,
-        Item: {
-          Id: resultId,
-          Type: 'SCREENSHOT', 
-          ResultName: action.actionName,
-          Progress: '未処理',
-          ResultSetId: resultSetId
-        }
-      }
-      await dynamodbDao.put(putObject);
-
-      // キューイングデータにID情報を追加
-      action.resultSetId = resultSetId;
-      action.resultId = resultId;
-
-      // Actionの内容をSQSに登録
-      await SQS.sendMessage({
-        MessageBody: JSON.stringify(action),
-        QueueUrl: SQS_QUEUE_URL,
-      }).promise();
-
-      result[resultSetId][action.actionId] = putObject.Item;
-
-    } catch (error) {
-      result[resultSetId][action.actionId] = error.message;
-    }
-  }
-
-  console.log(result);
-
-  try {
-    var response = {
-      'statusCode': 200,
-      'body': JSON.stringify({
-        message: result,
-      })
-    }
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-
-  return response
+  const diComponents = {
+    SQS: SQS,
+    DYNAMODB: DYNAMODB
+  };
+  return await screenshotProcessQueing({diComponents, event, context});
 }
