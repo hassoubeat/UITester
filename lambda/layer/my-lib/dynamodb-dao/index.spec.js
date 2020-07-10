5ff0fa534bfe138698a5a88e2e862e8a16845cda
hassoubeat <hassoubeat.work@gmail.com>
2020-07-10 18:19:02 +0900

    Add DynamoDBDaoのテストケース実装

---
+++ lambda/layer/my-lib/dynamodb-dao/index.spec.js
@@ -0,0 +1,86 @@
+const dynamoDao = require("./index");
+
+const AWS = require('aws-sdk');
+// const credentials = new AWS.SharedIniFileCredentials({ profile: 'localstack' });
+// AWS.config.credentials = credentials;
+const config = {
+  region: "ap-northeast-1",
+  endpoint: "http://localhost:4566"  
+}
+
+const dynamoDB = new AWS.DynamoDB(config);
+const dynamoDbDocumentClient = new AWS.DynamoDB.DocumentClient(config);
+const uitesterTableName = "local-uitester";
+
+describe('DynamoDB Dao Success Group', () => {
+
+  beforeAll( async () => {
+    console.log("beforeAll");
+
+    // テーブル作成
+    await createTestTable();
+    // 初期データ投入
+    const forGetTestObj = {
+      TableName: uitesterTableName,
+      Item: { Id: "for-get-test-object" }
+    }
+    await dynamoDbDocumentClient.put(forGetTestObj).promise();
+  });
+
+  // beforeEach( async () => {
+  // });
+
+  test('dynamoDao put test', async () => {
+    console.log("dynamoDao put test");
+    const response = await dynamoDao.put(
+      dynamoDbDocumentClient,
+      {
+        TableName: uitesterTableName,
+        Item: {
+          Id: "Result-Set-1",
+          Type: 'SCREENSHOT',
+          ProjectName: "Project-1"
+        }
+      }
+    );
+    expect(response).toEqual({"ConsumedCapacity":{"TableName":uitesterTableName,"CapacityUnits":1}});
+  });
+
+  // afterEach( async () => {
+  // });
+
+  afterAll( async () => {
+    console.log("afterAll");
+    await deleteTestTable();
+  });
+});
+
+async function createTestTable() {
+  var tableParams = {
+    TableName: uitesterTableName,
+    AttributeDefinitions: [
+      { AttributeName: 'Id', AttributeType: 'S'},
+      { AttributeName: 'ResultSetId', AttributeType: 'S' },
+      { AttributeName: 'ResultName', AttributeType: 'S' }
+    ],
+    KeySchema: [
+      { AttributeName: 'Id', KeyType: 'HASH' }
+    ],
+    GlobalSecondaryIndexes: [{
+      IndexName: "ResultSearchIndex",
+      KeySchema: [
+        { AttributeName: 'ResultSetId', KeyType: 'HASH' },
+        { AttributeName: 'ResultName', KeyType: 'RANGE' }
+      ],
+      Projection: { ProjectionType: "ALL" },
+      ProvisionedThroughput: { ReadCapacityUnits: 2, WriteCapacityUnits: 2 }
+    }],
+    ProvisionedThroughput: { ReadCapacityUnits: 2, WriteCapacityUnits: 2 }
+  };
+  resp = await dynamoDB.createTable(tableParams).promise();
+}
+
+async function deleteTestTable() {
+  const tableParams = { TableName: uitesterTableName }
+  await dynamoDB.deleteTable(tableParams).promise();
+}
