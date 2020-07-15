97b27e711b0480725a715b8c4a08d96f8aaeac73
hassoubeat <hassoubeat.work@gmail.com>
2020-07-15 23:23:44 +0900

    Update ResultSet一覧取得の実装

--- lambda/projects/GetResultSetListFunction/index.js
+++ lambda/projects/GetResultSetListFunction/index.js
@@ -1,15 +1,25 @@
 const AWS = require('aws-sdk');
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const dynamodbDao = require('dynamodb-dao');
 
 const UITESTER_DYNAMODB_TABLE_NAME = process.env.UITESTER_DYNAMODB_TABLE_NAME;
 
 exports.lambda_handler = async (event, context) => {
-  // TODO DynamoDBからScan & FilterでResultSetId一覧を取得
+  const resultSetList = await dynamodbDao.scan(
+    dynamoDB,
+    {
+      TableName: UITESTER_DYNAMODB_TABLE_NAME,
+      FilterExpression: "begins_with(Id, :result_sets_prefix)",
+      ExpressionAttributeValues: {
+        ":result_sets_prefix": "Result-Set-"
+      }
+    }
+  );
 
   var response = {
     'statusCode': 200,
     'body': JSON.stringify({
-      message: "test",
+      message: resultSetList.Items,
     })
   }
   return response
