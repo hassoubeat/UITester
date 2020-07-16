0ee18ea658ca534eda99b0f932e94dcb6e748e76
hassoubeat <hassoubeat.work@gmail.com>
2020-07-17 00:10:20 +0900

    Add  Result一覧取得処理の本実装

--- lambda/GetResultListFunction/index.js
+++ lambda/GetResultListFunction/index.js
@@ -5,10 +5,25 @@ const dynamodbDao = require('dynamodb-dao');
 const UITESTER_DYNAMODB_TABLE_NAME = process.env.UITESTER_DYNAMODB_TABLE_NAME;
 
 exports.lambda_handler = async (event, context) => {
+
+  const resultSetId = event.pathParameters.resultSetId;
+
+  const resultList = await dynamodbDao.query(
+    dynamoDB,
+    {
+      TableName: UITESTER_DYNAMODB_TABLE_NAME,
+      IndexName: "ResultSearchIndex",
+      KeyConditionExpression: "ResultSetId=:resultSetId",
+      ExpressionAttributeValues: {
+        ":resultSetId": resultSetId
+      }
+    }
+  );
+
   var response = {
     'statusCode': 200,
     'body': JSON.stringify({
-      message: "test",
+      message: resultList.Items,
     })
   }
   return response
---
+++ lambda/GetResultListFunction/index.spec.js
@@ -0,0 +1,61 @@
+const AWS = require('aws-sdk');
+const dynamoDbDao = require('dynamodb-dao');
+
+// 環境変数の設定
+process.env.UITESTER_DYNAMODB_TABLE_NAME = "dummy"
+
+const getResultListFunction = require("./index");
+
+// jestのマニュアルモック利用
+jest.mock('dynamodb-dao');
+
+describe('GetResultListFunction Success Group', () => {
+
+  // beforeAll( async () => {
+  //   console.log("beforeAll");
+  // });
+
+  beforeEach( async () => {
+    // モック処理のリセット
+    dynamoDbDao.query = async (dynamoDB, scanObj) => { 
+      return []; 
+    }
+  });
+
+  // Result一覧取得のユニットテスト
+  test('index.js test', async () => {
+    console.log("index.js test");
+
+    // マニュアルモックの上書き
+    dynamoDbDao.query = (dynamoDB, queryObj) => {
+      return {
+        Items: [ 
+          { ResultName: 'TOP画面', ResultSetId: 'Result-Set-2', Id: 'query-test-object-2' },
+          { ResultName: '詳細画面', ResultSetId: 'Result-Set-2', Id: 'query-test-object-3' }
+        ],
+        Count: 2,
+        ScannedCount: 2
+      };
+    }
+
+    const event = {
+      pathParameters: {
+        resultSetId: "Result-Set-2"
+      }
+    }
+    const response = await getResultListFunction.lambda_handler(event);
+    expect(JSON.parse(response.body).message).toEqual(
+      [
+        { ResultName: 'TOP画面', ResultSetId: 'Result-Set-2', Id: 'query-test-object-2' },
+        { ResultName: '詳細画面', ResultSetId: 'Result-Set-2', Id: 'query-test-object-3' }
+      ]
+    );
+  });
+
+  // afterEach( async () => {
+  // });
+
+  // afterAll( async () => {
+  //   console.log("afterAll");
+  // });
+});
