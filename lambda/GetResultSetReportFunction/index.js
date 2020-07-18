08c3de674985341a3011df3bdcb2a91910b51674
hassoubeat <hassoubeat.work@gmail.com>
2020-07-18 18:14:48 +0900

    Add コンソールレポート出力処理の実装

--- lambda/GetResultSetReportFunction/index.js
+++ lambda/GetResultSetReportFunction/index.js
@@ -1,7 +1,9 @@
 const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const dynamodbDao = require('dynamodb-dao');
+
 const dedent = require('dedent-js');
+const columnify = require('columnify');
 
 const UITESTER_DYNAMODB_TABLE_NAME = process.env.UITESTER_DYNAMODB_TABLE_NAME;
 
@@ -16,6 +18,17 @@ exports.lambda_handler = async (event, context) => {
   return response
 }
 
+// コンソールレポートの出力処理
+function outputConsoleReport(resultList) {
+  const sortedResultList = mapIdAscSort(resultList);
+  return columnify(sortedResultList, {
+    columns: ['Id', 'ResultName', 'Progress', 'Type', 'S3ObjectKey', 'OriginS3ObjectKey', 'TargetS3ObjectKey', 'ResultSetId'],
+    columnSplitter: ' | '
+  });
+}
+exports.outputConsoleReport = outputConsoleReport;
+
+// HTMLレポートの出力処理
 function outputHtmlReport() {
   return dedent`
   <!DOCTYPE html>
@@ -36,3 +49,13 @@ function outputHtmlReport() {
   `;
 }
 exports.outputHtmlReport = outputHtmlReport;
+
+// Map要素のIdの昇順でソートする
+function mapIdAscSort(mapList) {
+  return mapList.sort((a, b) => {
+    if(a.Id < b.Id) return -1;
+    if(a.Id > b.Id) return 1;
+    return 0;
+  });
+}
+exports.mapIdAscSort = mapIdAscSort;
