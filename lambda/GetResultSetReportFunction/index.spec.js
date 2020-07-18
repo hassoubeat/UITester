915067ec52886d1a17fbc4d46019b442c9d1c3f6
hassoubeat <hassoubeat.work@gmail.com>
2020-07-18 18:15:17 +0900

    Add コンソールレポート処理のテストコード実装

--- lambda/GetResultSetReportFunction/index.spec.js
+++ lambda/GetResultSetReportFunction/index.spec.js
@@ -19,6 +19,45 @@ describe('GetResultSetReportFunction Success Group', () => {
   // beforeEach( async () => {
   // });
 
+  // コンソールレポート出力処理のテスト
+  test('outputConsoleReport test', async () => {
+    console.log("outputConsoleReport test");
+
+    const resultList = [
+      {
+        "ResultName": "iPhone 6(横)",
+        "DiffResultDetail": "{\"isSameDimensions\":true,\"dimensionDifference\":{\"width\":0,\"height\":0},\"misMatchPercentage\":\"0.01\",\"analysisTime\":134}",
+        "OriginS3ObjectKey": "result/Result-Set-1/iPhone 6(横).png",
+        "ResultSetId": "Result-Set-18",
+        "TargetS3ObjectKey": "result/Result-Set-2/iPhone 6(横).png",
+        "Id": "Result-32",
+        "Type": "SCREENSHOT_DIFF",
+        "Progress": "処理済",
+        "S3ObjectKey": "result/Result-Set-18/iPhone 6(横).png"
+      },
+      {
+        "ResultName": "iPhone 6(縦)",
+        "DiffResultDetail": "{\"isSameDimensions\":false,\"dimensionDifference\":{\"width\":0,\"height\":-270},\"misMatchPercentage\":\"29.91\",\"analysisTime\":1221}",
+        "OriginS3ObjectKey": "result/Result-Set-1/iPhone 6(縦).png",
+        "ResultSetId": "Result-Set-18",
+        "TargetS3ObjectKey": "result/Result-Set-2/iPhone 6(縦).png",
+        "Id": "Result-31",
+        "Type": "SCREENSHOT_DIFF",
+        "Progress": "処理済",
+        "S3ObjectKey": "result/Result-Set-18/iPhone 6(縦).png"
+      }
+    ]
+
+    const report = getResultSetReportFunction.outputConsoleReport(resultList);
+
+    const correct = dedent`
+      ID        | RESULTNAME   | PROGRESS | TYPE            | S3OBJECTKEY                           | ORIGINS3OBJECTKEY                    | TARGETS3OBJECTKEY                    | RESULTSETID  
+      Result-31 | iPhone 6(縦) | 処理済   | SCREENSHOT_DIFF | result/Result-Set-18/iPhone 6(縦).png | result/Result-Set-1/iPhone 6(縦).png | result/Result-Set-2/iPhone 6(縦).png | Result-Set-18
+      Result-32 | iPhone 6(横) | 処理済   | SCREENSHOT_DIFF | result/Result-Set-18/iPhone 6(横).png | result/Result-Set-1/iPhone 6(横).png | result/Result-Set-2/iPhone 6(横).png | Result-Set-18
+    `
+    expect(report).toEqual(correct);
+  });
+
   // HTMLレポート出力処理のテスト
   test('outputHtmlReport test', async () => {
     console.log("outputHtmlReport test");
@@ -44,6 +83,24 @@ describe('GetResultSetReportFunction Success Group', () => {
     `;
 
     expect(report).toEqual(correct);
+  });
+
+  test('mapIdList test', async () => {
+    console.log("mapIdList test");
+
+    const mapIdList = [
+      { "Id": "Result-2", },
+      { "Id": "Result-3", },
+      { "Id": "Result-1", }
+    ]
+
+    const sortedMapList = getResultSetReportFunction.mapIdAscSort(mapIdList);
+
+    expect(sortedMapList).toEqual([
+      { "Id": "Result-1", },
+      { "Id": "Result-2", },
+      { "Id": "Result-3", }
+    ]);
   });
 
   // afterEach( async () => {
