ebd959ac8f629b705d8f3a5f6c155302506a5bfc
hassoubeat <hassoubeat.work@gmail.com>
2020-07-19 18:23:30 +0900

    Update HTMLレポート出力処理の本実装

--- lambda/GetResultSetReportFunction/index.js
+++ lambda/GetResultSetReportFunction/index.js
@@ -5,6 +5,7 @@ const dynamodbDao = require('dynamodb-dao');
 const dedent = require('dedent-js');
 const columnify = require('columnify');
 
+const UITESTER_S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 const UITESTER_DYNAMODB_TABLE_NAME = process.env.UITESTER_DYNAMODB_TABLE_NAME;
 
 exports.lambda_handler = async (event, context) => {
@@ -74,7 +75,31 @@ function outputConsoleReport(resultList) {
 exports.outputConsoleReport = outputConsoleReport;
 
 // TODO HTMLレポートの出力処理 (まだMock)
-function outputHtmlReport() {
+function outputHtmlReport(resultList) {
+  const sortedResultList = mapIdAscSort(resultList);
+
+  // ResultListをStringに変換する無名関数
+  const strConvertResultList = (list) => {
+    let output = "";
+    list.forEach((result) => {
+      const strResult = dedent`[
+        '${result.Id}',
+        '${result.ResultName}',
+        '${result.Progress}',
+        '${result.Type}',
+        '${result.DiffMisMatchRate}',
+        '${result.ResultSetId}',
+        {
+          origin: '${s3ObjectUrl(UITESTER_S3_BUCKET_NAME, result.OriginS3ObjectKey)}',
+          target: '${s3ObjectUrl(UITESTER_S3_BUCKET_NAME, result.TargetS3ObjectKey)}',
+          diffResult: '${s3ObjectUrl(UITESTER_S3_BUCKET_NAME, result.S3ObjectKey)}',
+        }
+      ]`
+      output = output + strResult + ","
+    })
+    return output;
+  }
+
   return dedent`
   <!DOCTYPE html>
   <html>
@@ -85,11 +110,91 @@ function outputHtmlReport() {
       <meta name="description" content="">
       <meta name="author" content="">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+      <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
+      <style>
+        .screenshot {
+          max-width: 100%;
+        }
+        .align-top {
+          vertical-align: top;
+        }
+        .gridjs-sort {
+          border: none;
+        }
+      </style>
     </head>
     <body>
-    Test
+      <div id="resultList"></div>
+
+      <!-- screenshotDiffModal -->
+      <div class="modal fade" id="screenshotDiffModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog modal-xl" role="document">
+          <div class="modal-content">
+            <div class="modal-body text-muted">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">ScreenshotDiff Detail</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div id="screenshotDiff"></div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/gridjs/dist/gridjs.development.js"></script>
+    <script>
+      var resultListGrid = new gridjs.Grid({
+        columns: ['Id', 'ResultName', 'Progress', 'Type', 'DiffMisMatchRate', 'ResultSetId', {
+          name: 'Diff',
+          sort: false,
+          formatter: (diffObj) => gridjs.html(\`<center><button class='btn btn-primary' onclick='showScreenshotDiff("\${diffObj.origin}", "\${diffObj.target}", "\${diffObj.diffResult}")' >Show Detail</button></center>\`)
+        }],
+        data: [${strConvertResultList(sortedResultList)}],
+        sort: true
+      }).render(document.getElementById('resultList'));;
+
+      var screenshotDiffGrid = new gridjs.Grid({
+        columns: [],
+        data: []
+      }).render(document.getElementById('screenshotDiff'));
+
+      function showScreenshotDiff(origin, target, diffResult) {
+        const diffContent = {
+          columns: [
+            {
+              name: 'Origin',
+              formatter: (img) => gridjs.html(\`<div><img class='screenshot' src='\${img}'/><div>\`)
+            },
+            {
+              name: 'Target',
+              formatter: (img) => gridjs.html(\`<div><img class='screenshot' src='\${img}'/></div>\`)
+            },
+            {
+              name: 'DiffResult',
+              formatter: (img) => gridjs.html(\`<div><img class='screenshot' src='\${img}'/></div>\`)
+            },
+          ],
+          data: [
+            [ origin, target, diffResult ]
+          ],
+          className: {
+            td: 'align-top'
+          }
+        }
+        screenshotDiffGrid.updateConfig(diffContent).forceRender(document.getElementById('screenshotDiff'));
+        $('#screenshotDiffModal').modal();
+      };
+    </script>
   </html>
   `;
 }
@@ -104,3 +209,8 @@ function mapIdAscSort(mapList) {
   });
 }
 exports.mapIdAscSort = mapIdAscSort;
+
+// S3のオブジェクトURLの取得
+function s3ObjectUrl(bucketName, objKey) {
+  return `https://${bucketName}.s3.amazonaws.com/${objKey}`
+}
--- lambda/GetResultSetReportFunction/index.spec.js
+++ lambda/GetResultSetReportFunction/index.spec.js
@@ -3,12 +3,39 @@ const dynamoDbDao = require('dynamodb-dao');
 const dedent = require('dedent-js');
 
 // 環境変数の設定
+process.env.S3_BUCKET_NAME = "dummy"
 process.env.UITESTER_DYNAMODB_TABLE_NAME = "dummy"
+
 
 const getResultSetReportFunction = require("./index");
 
 // jestのマニュアルモック利用
 jest.mock('dynamodb-dao');
+
+const resultList = [
+  {
+    "ResultName": "iPhone 6(横)",
+    "DiffMisMatchRate": "0.01",
+    "OriginS3ObjectKey": "result/Result-Set-1/iPhone 6(横).png",
+    "ResultSetId": "Result-Set-18",
+    "TargetS3ObjectKey": "result/Result-Set-2/iPhone 6(横).png",
+    "Id": "Result-32",
+    "Type": "SCREENSHOT_DIFF",
+    "Progress": "処理済",
+    "S3ObjectKey": "result/Result-Set-18/iPhone 6(横).png"
+  },
+  {
+    "ResultName": "iPhone 6(縦)",
+    "DiffMisMatchRate": "29.91",
+    "OriginS3ObjectKey": "result/Result-Set-1/iPhone 6(縦).png",
+    "ResultSetId": "Result-Set-18",
+    "TargetS3ObjectKey": "result/Result-Set-2/iPhone 6(縦).png",
+    "Id": "Result-31",
+    "Type": "SCREENSHOT_DIFF",
+    "Progress": "処理済",
+    "S3ObjectKey": "result/Result-Set-18/iPhone 6(縦).png"
+  }
+]
 
 describe('GetResultSetReportFunction Success Group', () => {
 
@@ -23,31 +50,6 @@ describe('GetResultSetReportFunction Success Group', () => {
   test('outputConsoleReport test', async () => {
     console.log("outputConsoleReport test");
 
-    const resultList = [
-      {
-        "ResultName": "iPhone 6(横)",
-        "DiffMisMatchRate": "0.01",
-        "OriginS3ObjectKey": "result/Result-Set-1/iPhone 6(横).png",
-        "ResultSetId": "Result-Set-18",
-        "TargetS3ObjectKey": "result/Result-Set-2/iPhone 6(横).png",
-        "Id": "Result-32",
-        "Type": "SCREENSHOT_DIFF",
-        "Progress": "処理済",
-        "S3ObjectKey": "result/Result-Set-18/iPhone 6(横).png"
-      },
-      {
-        "ResultName": "iPhone 6(縦)",
-        "DiffMisMatchRate": "29.91",
-        "OriginS3ObjectKey": "result/Result-Set-1/iPhone 6(縦).png",
-        "ResultSetId": "Result-Set-18",
-        "TargetS3ObjectKey": "result/Result-Set-2/iPhone 6(縦).png",
-        "Id": "Result-31",
-        "Type": "SCREENSHOT_DIFF",
-        "Progress": "処理済",
-        "S3ObjectKey": "result/Result-Set-18/iPhone 6(縦).png"
-      }
-    ]
-
     const report = getResultSetReportFunction.outputConsoleReport(resultList);
     const correct = dedent`
       ID        | RESULTNAME   | PROGRESS | TYPE            | DIFFMISMATCHRATE | S3OBJECTKEY                           | ORIGINS3OBJECTKEY                    | TARGETS3OBJECTKEY                    | RESULTSETID  
@@ -61,7 +63,7 @@ describe('GetResultSetReportFunction Success Group', () => {
   test('outputHtmlReport test', async () => {
     console.log("outputHtmlReport test");
 
-    const report = getResultSetReportFunction.outputHtmlReport();
+    const report = getResultSetReportFunction.outputHtmlReport(resultList);
 
     const correct = dedent`
     <!DOCTYPE html>
@@ -73,13 +75,118 @@ describe('GetResultSetReportFunction Success Group', () => {
         <meta name="description" content="">
         <meta name="author" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link href="https://unpkg.com/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
+        <style>
+          .screenshot {
+            max-width: 100%;
+          }
+          .align-top {
+            vertical-align: top;
+          }
+          .gridjs-sort {
+            border: none;
+          }
+        </style>
       </head>
       <body>
-      Test
+        <div id="resultList"></div>
+    
+        <!-- screenshotDiffModal -->
+        <div class="modal fade" id="screenshotDiffModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+          <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+              <div class="modal-body text-muted">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">ScreenshotDiff Detail</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div id="screenshotDiff"></div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </body>
+      <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/gridjs/dist/gridjs.development.js"></script>
+      <script>
+        var resultListGrid = new gridjs.Grid({
+          columns: ['Id', 'ResultName', 'Progress', 'Type', 'DiffMisMatchRate', 'ResultSetId', {
+            name: 'Diff',
+            sort: false,
+            formatter: (diffObj) => gridjs.html(\`<center><button class='btn btn-primary' onclick='showScreenshotDiff("\${diffObj.origin}", "\${diffObj.target}", "\${diffObj.diffResult}")' >Show Detail</button></center>\`)
+          }],
+          data: [[
+      'Result-31',
+      'iPhone 6(縦)',
+      '処理済',
+      'SCREENSHOT_DIFF',
+      '29.91',
+      'Result-Set-18',
+      {
+        origin: 'https://dummy.s3.amazonaws.com/result/Result-Set-1/iPhone 6(縦).png',
+        target: 'https://dummy.s3.amazonaws.com/result/Result-Set-2/iPhone 6(縦).png',
+        diffResult: 'https://dummy.s3.amazonaws.com/result/Result-Set-18/iPhone 6(縦).png',
+      }
+    ],[
+      'Result-32',
+      'iPhone 6(横)',
+      '処理済',
+      'SCREENSHOT_DIFF',
+      '0.01',
+      'Result-Set-18',
+      {
+        origin: 'https://dummy.s3.amazonaws.com/result/Result-Set-1/iPhone 6(横).png',
+        target: 'https://dummy.s3.amazonaws.com/result/Result-Set-2/iPhone 6(横).png',
+        diffResult: 'https://dummy.s3.amazonaws.com/result/Result-Set-18/iPhone 6(横).png',
+      }
+    ],],
+          sort: true
+        }).render(document.getElementById('resultList'));;
+    
+        var screenshotDiffGrid = new gridjs.Grid({
+          columns: [],
+          data: []
+        }).render(document.getElementById('screenshotDiff'));
+    
+        function showScreenshotDiff(origin, target, diffResult) {
+          const diffContent = {
+            columns: [
+              {
+                name: 'Origin',
+                formatter: (img) => gridjs.html(\`<div><img class='screenshot' src='\${img}'/><div>\`)
+              },
+              {
+                name: 'Target',
+                formatter: (img) => gridjs.html(\`<div><img class='screenshot' src='\${img}'/></div>\`)
+              },
+              {
+                name: 'DiffResult',
+                formatter: (img) => gridjs.html(\`<div><img class='screenshot' src='\${img}'/></div>\`)
+              },
+            ],
+            data: [
+              [ origin, target, diffResult ]
+            ],
+            className: {
+              td: 'align-top'
+            }
+          }
+          screenshotDiffGrid.updateConfig(diffContent).forceRender(document.getElementById('screenshotDiff'));
+          $('#screenshotDiffModal').modal();
+        };
+      </script>
     </html>
     `;
+    console.log(report);
 
     expect(report).toEqual(correct);
   });
