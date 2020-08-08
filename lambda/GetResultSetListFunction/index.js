0fb12e07e972e15592da7db578c51ed332997c18
hassoubeat <hassoubeat.work@gmail.com>
2020-08-08 12:58:31 +0900

    Update GetResultSetListFunction

--- lambda/GetResultSetListFunction/index.js
+++ lambda/GetResultSetListFunction/index.js
@@ -4,28 +4,44 @@ const dynamodbDao = require('dynamodb-dao');
 
 const UITESTER_DYNAMODB_TABLE_NAME = process.env.UITESTER_DYNAMODB_TABLE_NAME;
 
-exports.lambda_handler = async (event, context) => {
-  const resultSetList = await dynamodbDao.scan(
-    dynamoDB,
-    {
-      TableName: UITESTER_DYNAMODB_TABLE_NAME,
-      FilterExpression: "begins_with(Id, :result_sets_prefix)",
-      ExpressionAttributeValues: {
-        ":result_sets_prefix": "Result-Set-"
-      }
-    }
-  );
-
-  var response = {
-    'statusCode': 200,
-    'headers': {
-      "Access-Control-Allow-Headers" : "Content-Type",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "OPTIONS, GET"
-    },
-    'body': JSON.stringify({
-      message: resultSetList.Items,
-    })
+// レスポンス変数の定義
+var response = {
+  'statusCode': 200,
+  'headers': {
+    "Access-Control-Allow-Headers" : "*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS, GET"
   }
-  return response
+}
+
+exports.lambda_handler = async (event, context) => {
+
+  try {
+
+    const resultSetList = await dynamodbDao.scan(
+      dynamoDB,
+      {
+        TableName: UITESTER_DYNAMODB_TABLE_NAME,
+        FilterExpression: "begins_with(Id, :result_sets_prefix)",
+        ExpressionAttributeValues: {
+          ":result_sets_prefix": "Result-Set-"
+        }
+      }
+    );
+
+    response.body = JSON.stringify({
+      message: resultSetList.Items,
+    });
+
+  } catch (error) {
+    console.error(error);
+
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: error.message
+    });
+
+  } finally {
+    return response;
+  }
 }
